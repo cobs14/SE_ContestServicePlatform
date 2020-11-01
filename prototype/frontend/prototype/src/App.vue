951ef56fdf0050106c109be6526aceb855f03873
cobs14 <44751106+cobs14@users.noreply.github.com>
2020-11-01 16:31:14 +0800

    Revert "New depencies and re-arranged project structure"
    This reverts commit 016683753c9c030d222906093913f70a418ff4ba.

--- prototype/frontend/prototype/src/App.vue
+++ prototype/frontend/prototype/src/App.vue
@@ -1,23 +1,26 @@
 <template>
   <div id="app">
-    <v-app>
-      <v-header> </v-header>
-      <infocard> </infocard>
-      <router-view />
-      <v-footer> </v-footer>
-    </v-app>
+    <v-card>
+      This is generated using Vuetify.
+       </v-card>
+    <img src="./assets/logo.png">
+    <router-view/>
   </div>
 </template>
 
 <script>
-/* eslint-disable */
-import vHeader from "@/components/Header.vue";
 export default {
-  components: {
-    vHeader,
-  },
-  name: "App",
-};
+  name: 'App'
+}
 </script>
 
-
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
--- prototype/frontend/prototype/src/router/index.js
+++ prototype/frontend/prototype/src/router/index.js
@@ -1,18 +1,15 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HomePage from '@/views/HomePage'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
-  // Todo: 与后端配合时，请参考下列网址（以解决可能的404问题）
-  // https://router.vuejs.org/zh/guide/essentials/history-mode.html#%E5%90%8E%E7%AB%AF%E9%85%8D%E7%BD%AE%E4%BE%8B%E5%AD%90
-  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HomePage',
-      component: HomePage
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
