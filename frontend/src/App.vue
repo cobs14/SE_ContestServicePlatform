69bb13042dd691a6a1bcf5e5d27e8219b12cef88
cobs14 <44751106+cobs14@users.noreply.github.com>
2020-11-12 14:52:58 +0800

    snackbar + register page seperation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,8 +2,21 @@
   <div id="app">
     <v-app>
       <v-header> </v-header>
+      <v-snackbar
+        v-model="snackbar.show"
+        :timeout="3000"
+        top
+        :color="snackbar.color"
+      >
+        {{ snackbar.message }}
+        <template v-slot:action="{ attrs }">
+          <v-btn dark text v-bind="attrs" @click="snackbar.show = false">
+            Close
+          </v-btn>
+        </template>
+      </v-snackbar>
       <v-main>
-        <router-view />
+        <router-view @showSnackbar="showSnackbar" />
       </v-main>
       <v-footer> </v-footer>
     </v-app>
@@ -19,5 +32,18 @@ export default {
     vHeader,
   },
   name: "App",
+  data() {
+    return {
+      snackbar: { show: false, color: "success", message: "" },
+    };
+  },
+  methods: {
+    showSnackbar: function (arg) {
+      //console.log(arg.message, arg.color);
+      this.snackbar.message = arg.message;
+      this.snackbar.color = arg.color;
+      this.snackbar.show = true;
+    },
+  },
 };
 </script>
