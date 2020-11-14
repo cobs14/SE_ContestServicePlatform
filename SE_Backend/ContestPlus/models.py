f228645a7d1e1895bc839b44fd3fd0d6958fee5f
Lucius <souzou@foxmail.com>
2020-11-14 09:44:28 +0800

    Update models

--- SE_Backend/ContestPlus/models.py
+++ SE_Backend/ContestPlus/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=32)
+    password = models.CharField(max_length=256)
+    email = models.EmailField(max_length=64)
+    loginStatus = models.BooleanField()
+    qualificationStatus = models.CharField(max_length=16)
+    documentNumberNeeded = models.BooleanField()
+    documentNumber = models.BooleanField()
+    trueName = models.CharField(max_length=32)
+    birthTime = models.DateField()
+
+
+class Sponsor(models.Model):
+    sponsorName = models.CharField(max_length=64)
+    password = models.CharField(max_length=256)
+    email = models.EmailField(max_length=64)
+    loginStatus = models.BooleanField()
+    checkStatus = models.CharField(max_length=64)
+
+
+class EmailCode(models.Model):
+    userType = models.CharField(max_length=16)
+    userId = models.IntegerField(max_length=16)
+    code = models.CharField(max_length=6)
+    sendTime = models.DateField()
+
+
+
