f69b16ec0d2254a5fc354dacdf13d014ad49060d
cobs14 <44751106+cobs14@users.noreply.github.com>
2020-11-26 23:31:22 +0800

    sending pics (undone)

--- frontend/src/network/request.js
+++ frontend/src/network/request.js
@@ -29,3 +29,33 @@ export function requestPost(config, jwt = null) {
         })
     })
 }
+
+export function requestUploadPictures(config, jwt = null) {
+    return new Promise((resolve, reject) => {
+        //创建axios实例
+        const instance = axios.create({
+            baseURL: '/api/handlepic/upload',
+            method: "post",
+            transformRequest: [function (data) {
+                // 传输文件和图片
+                let formData = new window.FormData();
+                formData.append('config', JSON.stringify(data['config']));
+                for (let key in data) {
+                    formData.append(key, data[key]);
+                }
+                console.log('origin data:', data, 'form data:', formData, formData.get('config'));
+                return formData;
+            }],
+            headers: (jwt == null ? { 'Content-Type': 'multipart/form-data', } : {
+                'Content-Type': 'multipart/form-data',
+                'jwt': jwt
+            }),
+        })
+        // 发送网络请求
+        instance(config).then(res => {
+            resolve(res)
+        }).catch(err => {
+            reject(err)
+        })
+    })
+}
