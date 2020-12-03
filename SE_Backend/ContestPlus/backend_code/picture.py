1321ad9297411e7538f1e9777d7c43e1fa597de3
Li Yunxuan <52957393+LiXCloudland@users.noreply.github.com>
2020-12-03 17:38:32 +0800

    delete comment

--- SE_Backend/ContestPlus/backend_code/picture.py
+++ SE_Backend/ContestPlus/backend_code/picture.py
@@ -137,14 +137,3 @@ def apiHandlePicView(request):
         return JsonResponse(return_data)
     return JsonResponse({'error': 'need POST method'})
 
-
-# def upload_file(request):
-#     if request.method == "POST":    # 请求方法为POST时，进行处理
-#         myFile =request.FILES.get("myfile", None)    # 获取上传的文件，如果没有文件，则默认为None
-#         if not myFile:
-#             returnHttpResponse("no files for upload!")
-#         destination = open(os.path.join("E:\\upload",myFile.name),'wb+')    # 打开特定的文件进行二进制的写操作
-#         for chunk in myFile.chunks():      # 分块写入文件
-#             destination.write(chunk)
-#         destination.close()
-#         returnHttpResponse("upload over!")
