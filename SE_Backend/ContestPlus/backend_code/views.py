7ae894ae67fbac31cd24dd2c54ee8780df4e1ef3
Lucius <souzou@foxmail.com>
2020-12-15 00:05:54 +0800

    fix bug

--- SE_Backend/ContestPlus/backend_code/views.py
+++ SE_Backend/ContestPlus/backend_code/views.py
@@ -322,6 +322,7 @@ def apiResetPassword(request):
                 user = User.objects.get(id=email_code.userId)
                 user.password = post['password']
                 user.save()
+                email_code.delete()
                 return response
             except EmailCode.DoesNotExist:
                 return JsonResponse({'error': 'no such a code'})
