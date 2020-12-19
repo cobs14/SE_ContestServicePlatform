20671867cd3c7762e1653664dfc8399b5d1c821f
Li Yunxuan <52957393+LiXCloudland@users.noreply.github.com>
2020-12-19 08:20:11 +0800

    update certificate

--- SE_Backend/ContestPlus/backend_code/certificate.py
+++ SE_Backend/ContestPlus/backend_code/certificate.py
@@ -1,8 +1,12 @@
-
+from PIL import Image
+from SE_Backend import settings
 
 def apiCertificationGet(request):
+    image=Image.open(str(settings.BASE_DIR)+"/CertificationModel/certification_model.png")
+    image.show()
     return
 
 
 def apiCertificationVerify(request):
+
     return
