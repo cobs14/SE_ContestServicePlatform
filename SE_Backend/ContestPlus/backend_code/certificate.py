3baca2b62942daa591c1c041c10fa4011ac6f508
Li Yunxuan <52957393+LiXCloudland@users.noreply.github.com>
2020-12-19 08:26:35 +0800

    update invitationcode

--- SE_Backend/ContestPlus/backend_code/certificate.py
+++ SE_Backend/ContestPlus/backend_code/certificate.py
@@ -3,6 +3,8 @@ from SE_Backend import settings
 
 def apiCertificationGet(request):
     image=Image.open(str(settings.BASE_DIR)+"/CertificationModel/certification_model.png")
+
+
     image.show()
     return
 
