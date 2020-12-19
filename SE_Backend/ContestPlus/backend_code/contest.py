e2f3f61ce50a42a82cd966c20d4572bee71fb1bc
Li Yunxuan <52957393+LiXCloudland@users.noreply.github.com>
2020-12-19 19:03:31 +0800

    Update contest.py

--- SE_Backend/ContestPlus/backend_code/contest.py
+++ SE_Backend/ContestPlus/backend_code/contest.py
@@ -128,8 +128,7 @@ def api_contest_creation(request):
         if us_type != 'sponsor':
             return JsonResponse({'error': 'authority'})
         contest = Contest(title=post['title'], description=post['description'],
-                          # module=json.dumps(post['module']),
-                          module=post['module'],
+                          module=json.dumps(post['module']).decode("unicode-escape"),
                           allowGroup=post['allowGroup'], sponsorId=user.id,
                           applyStartTime=post['applyStartTime'],
                           applyDeadline=post['applyDeadline'],
