10a3fd46f77b6b2cd1442763a06e5574f962c9d9
Lucius <souzou@foxmail.com>
2020-12-20 11:46:56 +0800

    fix bug

--- SE_Backend/ContestPlus/backend_code/certificate.py
+++ SE_Backend/ContestPlus/backend_code/certificate.py
@@ -78,23 +78,23 @@ def apiCertificationGet(request):
                 draw.text(contest_position_part1, contest_text_part1, font=contest_font_part1, fill='white')
 
                 contest_position_part2 = (1000 - 25 * (len(contest_text_part2)), 710)
-                contest_font_part2 = ImageFont.truetype(settings.BASE_DIR) + '/CertificationModel/STZHONGS.TTF', 50)
+                contest_font_part2 = ImageFont.truetype(str(settings.BASE_DIR) + '/CertificationModel/STZHONGS.TTF', 50)
                 draw.text(contest_position_part2, contest_text_part2, font=contest_font_part2, fill='white')
 
                 award_position = (1000 - 50 * (len(award_text)), 780)
-                award_font = ImageFont.truetype(settings.BASE_DIR) + '/CertificationModel/STZHONGS.TTF', 100)
+                award_font = ImageFont.truetype(str(settings.BASE_DIR) + '/CertificationModel/STZHONGS.TTF', 100)
                 draw.text(award_position, award_text, font=award_font, fill='white')
 
                 sponsor_position = (1600 - 18 * (len(sponsor_text)), 1100)
-                sponsor_font = ImageFont.truetype(settings.BASE_DIR) + '/CertificationModel/STZHONGS.TTF', 36)
+                sponsor_font = ImageFont.truetype(str(settings.BASE_DIR) + '/CertificationModel/STZHONGS.TTF', 36)
                 draw.text(sponsor_position, sponsor_text, font=sponsor_font, fill='white')
 
                 time_position = (1600 - 18 * (len(time_text) - 3), 1150)
-                time_font = ImageFont.truetype(settings.BASE_DIR) + '/CertificationModel/STZHONGS.TTF', 36)
+                time_font = ImageFont.truetype(str(settings.BASE_DIR) + '/CertificationModel/STZHONGS.TTF', 36)
                 draw.text(time_position, time_text, font=time_font, fill='white')
 
                 qrcode_position = (1005 - 18 * (len(qrcode_text)), 950)
-                qrcode_font = ImageFont.truetype(settings.BASE_DIR) + '/CertificationModel/STZHONGS.TTF', 36)
+                qrcode_font = ImageFont.truetype(str(settings.BASE_DIR) + '/CertificationModel/STZHONGS.TTF', 36)
                 draw.text(qrcode_position, qrcode_text, font=qrcode_font, fill='white')
                 qr = qrcode.QRCode(version=5, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4, )
                 qr.add_data(host_name+i.verifyCode)
