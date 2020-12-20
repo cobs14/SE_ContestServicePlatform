374ecf41e2fd389948a2bad0df3fb4def981287e
Lucius <souzou@foxmail.com>
2020-12-21 01:54:21 +0800

    Update certificate.py

--- SE_Backend/ContestPlus/backend_code/certificate.py
+++ SE_Backend/ContestPlus/backend_code/certificate.py
@@ -9,6 +9,71 @@ import qrcode
 import os
 from SE_Backend import settings
 
+def generate(award, user, sponsor, contest, index, zip_file, verifyCode):
+    image = Image.open(
+        str(settings.BASE_DIR) + "/CertificationModel/certification_model.png")
+    draw = ImageDraw.Draw(image)
+    name_text = user.trueName + ":"
+    contest_text_part1 = "恭喜你在" + sponsor.trueName + "举办的"
+    contest_text_part2 = contest.title + "中获得"
+    award_text = award
+    sponsor_text = sponsor.trueName
+    time_text = contest.publishResult
+    qrcode_text = "扫描二维码验证："
+    host_name = settings.host + '/certificate/'
+
+    name_position = (380 - 36 * (len(name_text) - 1), 565)
+    name_font = ImageFont.truetype(
+        str(settings.BASE_DIR) + '/CertificationModel/STXINWEI.TTF', 72)
+    draw.text(name_position, name_text, font=name_font, fill='white')
+
+    contest_position_part1 = (1000 - 25 * (len(contest_text_part1)), 640)
+    contest_font_part1 = ImageFont.truetype(
+        str(settings.BASE_DIR) + '/CertificationModel/STZHONGS.TTF', 50)
+    draw.text(contest_position_part1, contest_text_part1,
+              font=contest_font_part1, fill='white')
+
+    contest_position_part2 = (1000 - 25 * (len(contest_text_part2)), 710)
+    contest_font_part2 = ImageFont.truetype(
+        str(settings.BASE_DIR) + '/CertificationModel/STZHONGS.TTF', 50)
+    draw.text(contest_position_part2, contest_text_part2,
+              font=contest_font_part2, fill='white')
+
+    award_position = (1000 - 50 * (len(award_text)), 780)
+    award_font = ImageFont.truetype(
+        str(settings.BASE_DIR) + '/CertificationModel/STZHONGS.TTF', 100)
+    draw.text(award_position, award_text, font=award_font, fill='white')
+
+    sponsor_position = (1600 - 18 * (len(sponsor_text)), 1100)
+    sponsor_font = ImageFont.truetype(
+        str(settings.BASE_DIR) + '/CertificationModel/STZHONGS.TTF', 36)
+    draw.text(sponsor_position, sponsor_text, font=sponsor_font, fill='white')
+
+    time_position = (1600 - 18 * (len(time_text) - 3), 1150)
+    time_font = ImageFont.truetype(
+        str(settings.BASE_DIR) + '/CertificationModel/STZHONGS.TTF', 36)
+    draw.text(time_position, time_text, font=time_font, fill='white')
+
+    qrcode_position = (1005 - 18 * (len(qrcode_text)), 950)
+    qrcode_font = ImageFont.truetype(
+        str(settings.BASE_DIR) + '/CertificationModel/STZHONGS.TTF', 36)
+    draw.text(qrcode_position, qrcode_text, font=qrcode_font, fill='white')
+    qr = qrcode.QRCode(version=5,
+                       error_correction=qrcode.constants.ERROR_CORRECT_L,
+                       box_size=10, border=4, )
+    qr.add_data(host_name + verifyCode)
+    qr.make(fit=True)
+    qr_img = qr.make_image()
+    qr_img = qr_img.resize((200, 200))
+    image.paste(qr_img, (900, 1020))
+    image_dir = str(settings.BASE_DIR) + "/files/needPermission/" \
+                                         "certificate/" + str(contest.id) \
+                + '/' + str(user.id) + '_' + user.trueName + '/'
+    if not os.path.exists(image_dir):
+        os.makedirs(image_dir)
+    image.save(image_dir + str(index) + '.png')
+    zip_file.write(image_dir + str(index) + '.png', str(index) + '.png')
+
 
 def apiCertificationGet(request):
     if request.method == 'POST':
@@ -48,8 +113,7 @@ def apiCertificationGet(request):
                       str(contest.id) + "/"
             if not os.path.exists(zip_dir):
                 os.makedirs(zip_dir)
-            zip_name = zip_dir + str(user.id) + '_' + user.trueName + '.zip'
-
+            zip_name = zip_dir + str(user.id) + '.zip'
             if i.verifyCode == '':
                 i.verifyCode = random_str(32)
                 i.save()
@@ -59,57 +123,8 @@ def apiCertificationGet(request):
                 continue
             zip_file = zipfile.ZipFile(zip_name, "w", zipfile.ZIP_DEFLATED)
             for j, award in enumerate(imgs):
-                image = Image.open(str(settings.BASE_DIR) + "/CertificationModel/certification_model.png")
-                draw = ImageDraw.Draw(image)
-                name_text = user.trueName + ":"
-                contest_text_part1 = "恭喜你在" + sponsor.trueName + "举办的"
-                contest_text_part2 = contest.title + "中获得"
-                award_text = award
-                sponsor_text = sponsor.trueName
-                time_text = contest.publishResult
-                qrcode_text = "扫描二维码验证："
-                host_name = settings.host + '/certificate/'
-
-                name_position = (380 - 36 * (len(name_text) - 1), 565)
-                name_font = ImageFont.truetype(str(settings.BASE_DIR) + '/CertificationModel/STXINWEI.TTF', 72)
-                draw.text(name_position, name_text, font=name_font, fill='white')
-
-                contest_position_part1 = (1000 - 25 * (len(contest_text_part1)), 640)
-                contest_font_part1 = ImageFont.truetype(str(settings.BASE_DIR) + '/CertificationModel/STZHONGS.TTF', 50)
-                draw.text(contest_position_part1, contest_text_part1, font=contest_font_part1, fill='white')
-
-                contest_position_part2 = (1000 - 25 * (len(contest_text_part2)), 710)
-                contest_font_part2 = ImageFont.truetype(str(settings.BASE_DIR) + '/CertificationModel/STZHONGS.TTF', 50)
-                draw.text(contest_position_part2, contest_text_part2, font=contest_font_part2, fill='white')
-
-                award_position = (1000 - 50 * (len(award_text)), 780)
-                award_font = ImageFont.truetype(str(settings.BASE_DIR) + '/CertificationModel/STZHONGS.TTF', 100)
-                draw.text(award_position, award_text, font=award_font, fill='white')
-
-                sponsor_position = (1600 - 18 * (len(sponsor_text)), 1100)
-                sponsor_font = ImageFont.truetype(str(settings.BASE_DIR) + '/CertificationModel/STZHONGS.TTF', 36)
-                draw.text(sponsor_position, sponsor_text, font=sponsor_font, fill='white')
-
-                time_position = (1600 - 18 * (len(time_text) - 3), 1150)
-                time_font = ImageFont.truetype(str(settings.BASE_DIR) + '/CertificationModel/STZHONGS.TTF', 36)
-                draw.text(time_position, time_text, font=time_font, fill='white')
-
-                qrcode_position = (1005 - 18 * (len(qrcode_text)), 950)
-                qrcode_font = ImageFont.truetype(str(settings.BASE_DIR) + '/CertificationModel/STZHONGS.TTF', 36)
-                draw.text(qrcode_position, qrcode_text, font=qrcode_font, fill='white')
-                qr = qrcode.QRCode(version=5, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4, )
-                qr.add_data(host_name+i.verifyCode)
-                qr.make(fit=True)
-                qr_img = qr.make_image()
-                qr_img = qr_img.resize((200, 200))
-                image.paste(qr_img, (900, 1020))
-                image_dir = str(settings.BASE_DIR) + "/files/needPermission/" \
-                                                     "certificate/"+str(contest.id)\
-                                                    + '/' + str(user.id) + '_' + user.trueName + '/'
-                if not os.path.exists(image_dir):
-                    os.makedirs(image_dir)
-                image.save(image_dir + str(j) + '.png')
-                zip_file.write(image_dir + str(j) + '.png', str(j) + '.png')
+                generate(award, user, sponsor, contest, j, zip_file,
+                         i.verifyCode)
             zip_file.close()
             outer_zip_file.write(zip_name, str(user.id) + '_' + user.trueName + '.zip')
         outer_zip_file.close()
@@ -118,6 +133,52 @@ def apiCertificationGet(request):
             contest.id) + ".zip"
         response['Content-Type'] = 'application/octet-stream'
         response['X-Accel-Redirect'] = '/file/certificate/' + str(contest.id) + ".zip"
+        return response
+    return
+
+
+def apiCertificationGetMy(request):
+    if request.method == 'POST':
+        post = eval(request.body)
+        us_type, user = user_type(request)
+        if us_type == 'error':
+            return JsonResponse({'error': 'login'})
+        if us_type != 'user':
+            return JsonResponse({'error': 'authority'})
+        try:
+            contest = Contest.objects.get(id=post['contestId'])
+            if contest.censorStatus != 'accept' or contest.publishResult == '':
+                return JsonResponse({'error': 'status'})
+        except Contest.DoesNotExist:
+            return JsonResponse({'error': 'contest'})
+        retrieved_participant = Participation.objects\
+            .filter(targetContestId=contest.id, userId=user.id)
+        for i in retrieved_participant[0: 1]:
+            user = User.objects.get(id=i.userId)
+            imgs = [i.mainAward] + (i.extraAward.split(' ') if len(i.extraAward) > 0 else [])
+            if len(imgs) == 0:
+                break
+            zip_dir = str(settings.BASE_DIR) + "/files/needPermission/certificate/" +\
+                      str(contest.id) + "/"
+            if not os.path.exists(zip_dir):
+                os.makedirs(zip_dir)
+            zip_name = zip_dir + str(user.id) + '.zip'
+            if i.verifyCode == '':
+                i.verifyCode = random_str(32)
+                i.save()
+            else:
+                break
+            zip_file = zipfile.ZipFile(zip_name, "w", zipfile.ZIP_DEFLATED)
+            sponsor = User.objects.get(id=contest.sponsorId)
+            for j, award in enumerate(imgs):
+                generate(award, user, sponsor, contest, j, zip_file,
+                         i.verifyCode)
+            zip_file.close()
+        response = HttpResponse(status=200)
+        response['Content-Disposition'] = 'attachment; filename=%s' % str(
+            user.id) + ".zip"
+        response['Content-Type'] = 'application/octet-stream'
+        response['X-Accel-Redirect'] = '/file/certificate/' + str(contest.id) + '/' + str(user.id) + '.zip'
         return response
     return
 
