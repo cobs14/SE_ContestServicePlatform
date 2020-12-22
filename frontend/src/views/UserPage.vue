7c4d697331c1b8589be793ace3b39b23f08146da
yenjuc <48012444+yenjuc@users.noreply.github.com>
2020-12-23 01:48:12 +0800

    Fix User Page avatar bug

--- frontend/src/views/UserPage.vue
+++ frontend/src/views/UserPage.vue
@@ -3,42 +3,42 @@
     <v-card class="ma-2 pa-2">
     <v-row>
       <v-col cols="12" sm="4">
-            <v-card
-              elevation="0"
-              max-width="220px"
-              max-height="220px"
-              class="pl-5 pt-5"
-            >
-              <v-img
-                v-if="userInfo.avatar && userInfo.avatar != ''"
-                :src="avatar"
-                @error="avatar = undefined"
-                max-width="220px"
-                max-height="220px"
-              >
-              </v-img>
-              <v-img
-                v-else
-                :src="defaultHead"
-                max-width="220px"
-                max-height="220px"
-              >
-              </v-img>
-            </v-card>
-            <v-card-title style="font-weight: 800" class="text-h5">
-              {{ showName() }}
-            </v-card-title>
-            <v-card-text v-if="userInfo.userType !== 'sponsor'">
-              <div class="grey--text">就读院校：{{ userInfo.school || "暂无" }}</div>
-              <div class="grey--text">就读专业：{{ userInfo.major || "暂无" }}</div>
-              <div class="grey--text">电子邮箱：{{ userInfo.email || "暂无" }}</div>
-              <div class="grey--text">
-                个人简介：{{ userInfo.description || "暂无" }}
-              </div>
-            </v-card-text>
-            <v-card-text v-if="userInfo.userType === 'sponsor'">
-              <div class="grey--text">联系邮箱：{{ userInfo.email || "暂无" }}</div>
-            </v-card-text>
+        <v-card
+          elevation="0"
+          max-width="220px"
+          max-height="220px"
+          class="pl-5 pt-5"
+        >
+          <v-img
+            v-if="userInfo.avatar && userInfo.avatar != ''"
+            :src="userInfo.avatar"
+            @error="userInfo.avatar = undefined"
+            max-width="220px"
+            max-height="220px"
+          >
+          </v-img>
+          <v-img
+            v-else
+            :src="defaultHead"
+            max-width="220px"
+            max-height="220px"
+          >
+          </v-img>
+        </v-card>
+        <v-card-title style="font-weight: 800" class="text-h5">
+          {{ showName() }}
+        </v-card-title>
+        <v-card-text v-if="userInfo.userType !== 'sponsor'">
+          <div class="grey--text">就读院校：{{ userInfo.school || "暂无" }}</div>
+          <div class="grey--text">就读专业：{{ userInfo.major || "暂无" }}</div>
+          <div class="grey--text">电子邮箱：{{ userInfo.email || "暂无" }}</div>
+          <div class="grey--text">
+            个人简介：{{ userInfo.description || "暂无" }}
+          </div>
+        </v-card-text>
+        <v-card-text v-if="userInfo.userType === 'sponsor'">
+          <div class="grey--text">联系邮箱：{{ userInfo.email || "暂无" }}</div>
+        </v-card-text>
       </v-col>
       <v-col cols="12" sm="8">
         <v-card-subtitle class="text-h6 mt-6">和 {{showName()}} 聊天</v-card-subtitle>
