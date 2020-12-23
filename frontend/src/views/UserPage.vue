26725c2750cca2456ea05fb1b980e74e0281b3a3
yenjuc <48012444+yenjuc@users.noreply.github.com>
2020-12-23 16:26:37 +0800

    Add Chat Box User Type Check (guest)

--- frontend/src/views/UserPage.vue
+++ frontend/src/views/UserPage.vue
@@ -7,52 +7,67 @@
         </v-skeleton-loader>
     </div>
     <div v-if="!isLoading">
+      <v-alert
+        v-if="userType == 'guest'"
+        prominent
+        type="warning"
+        border="left"
+      >
+        <v-row align="center">
+          <v-col class="grow">
+            您需要先前往个人中心进行实名验证后才能使用私信功能
+          </v-col>
+          <v-col class="shrink">
+            <v-btn @click="redirect('/user')" outlined>前往认证页面</v-btn>
+          </v-col>
+        </v-row>
+      </v-alert>
       <v-card class="ma-2 pa-2">
-    <v-row>
-      <v-col cols="12" sm="4">
-        <v-card
-          elevation="0"
-          max-width="220px"
-          max-height="220px"
-          class="pl-5 pt-5"
-        >
-          <v-img
-            v-if="userInfo.avatar && userInfo.avatar != ''"
-            :src="userInfo.avatar"
-            @error="userInfo.avatar = undefined"
-            max-width="220px"
-            max-height="220px"
-          >
-          </v-img>
-          <v-img
-            v-else
-            :src="defaultHead"
-            max-width="220px"
-            max-height="220px"
-          >
-          </v-img>
-        </v-card>
-        <v-card-title style="font-weight: 800" class="text-h5">
-          {{ showName() }}
-        </v-card-title>
-        <v-card-text v-if="userInfo.userType !== 'sponsor'">
-          <div class="grey--text">就读院校：{{ userInfo.school || "暂无" }}</div>
-          <div class="grey--text">就读专业：{{ userInfo.major || "暂无" }}</div>
-          <div class="grey--text">电子邮箱：{{ userInfo.email || "暂无" }}</div>
-          <div class="grey--text">
-            个人简介：{{ userInfo.description || "暂无" }}
-          </div>
-        </v-card-text>
-        <v-card-text v-if="userInfo.userType === 'sponsor'">
-          <div class="grey--text">联系邮箱：{{ userInfo.email || "暂无" }}</div>
-        </v-card-text>
-      </v-col>
-      <v-col cols="12" sm="8">
-        <v-card-subtitle class="text-h6 mt-6">和 {{showName()}} 聊天</v-card-subtitle>
-        <v-divider></v-divider>
-        <chat-box :contactInfo="userInfo"></chat-box>
-      </v-col>
-    </v-row>
+        <v-row>
+          <v-col cols="12" sm="4">
+            <v-card
+              elevation="0"
+              max-width="220px"
+              max-height="220px"
+              class="pl-5 pt-5"
+            >
+              <v-img
+                v-if="userInfo.avatar && userInfo.avatar != ''"
+                :src="userInfo.avatar"
+                @error="userInfo.avatar = undefined"
+                max-width="220px"
+                max-height="220px"
+              >
+              </v-img>
+              <v-img
+                v-else
+                :src="defaultHead"
+                max-width="220px"
+                max-height="220px"
+              >
+              </v-img>
+            </v-card>
+            <v-card-title style="font-weight: 800" class="text-h5">
+              {{ showName() }}
+            </v-card-title>
+            <v-card-text v-if="userInfo.userType !== 'sponsor'">
+              <div class="grey--text">就读院校：{{ userInfo.school || "暂无" }}</div>
+              <div class="grey--text">就读专业：{{ userInfo.major || "暂无" }}</div>
+              <div class="grey--text">电子邮箱：{{ userInfo.email || "暂无" }}</div>
+              <div class="grey--text">
+                个人简介：{{ userInfo.description || "暂无" }}
+              </div>
+            </v-card-text>
+            <v-card-text v-if="userInfo.userType === 'sponsor'">
+              <div class="grey--text">联系邮箱：{{ userInfo.email || "暂无" }}</div>
+            </v-card-text>
+          </v-col>
+          <v-col cols="12" sm="8">
+            <v-card-subtitle class="text-h6 mt-6">和 {{showName()}} 聊天</v-card-subtitle>
+            <v-divider></v-divider>
+            <chat-box :contactInfo="userInfo"></chat-box>
+          </v-col>
+        </v-row>
       </v-card>
     </div>
   </v-container>
@@ -115,12 +130,14 @@ export default {
       this.redirect("/user");
     } else {
       this.fetchUserInfo();
-    }    
+    } 
+    this.userType = this.getUserType();   
   },
   data() {
     return {
       userId: 0,
       userInfo: Object,
+      userType: '',
       isLoading: true,
       defaultHead: require("../../static/images/defaultHead.jpg")
     };
