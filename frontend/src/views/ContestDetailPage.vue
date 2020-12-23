c8326ae2dd9f97ce73446c6b2eb35dcfdbb2c3eb
yenjuc <48012444+yenjuc@users.noreply.github.com>
2020-12-23 18:12:33 +0800

    Add User Center Guest Check

--- frontend/src/views/ContestDetailPage.vue
+++ frontend/src/views/ContestDetailPage.vue
@@ -192,8 +192,7 @@
                   <div>{{ info.sponsorEmail }}</div>
                 </v-card-text>
                 <v-card-text>
-                  <div><b>站内私信</b></div>
-                  <v-btn outlined block color="info" @click="external('/user/' + info.sponsorId)">点此交谈</v-btn>
+                  <v-btn outlined block color="info" @click="external('/user/' + info.sponsorId)">点击前往站内交流</v-btn>
                 </v-card-text>
                 <v-chip class="ma-2" color="orange" label text-color="white">
                   <v-icon class="material-icons mr-1">mdi-clock</v-icon>
--- frontend/src/views/UserCenterPage.vue
+++ frontend/src/views/UserCenterPage.vue
@@ -36,64 +36,72 @@
       <div style="margin: 0; background: white; width: 100%; height: 80px">
         <v-breadcrumbs :items="paths" divider="-"></v-breadcrumbs>
       </div>
-      <v-container v-if="page === 'info'">
-        <user-card :info="userInfo" @showSnackbar="snackbar"></user-card>
-        <user-info-manager
-          v-if="panels.infoPanel"
-          :info="userInfo"
-          @showSnackbar="snackbar"
-        />
-        <user-password-manager
-          v-if="panels.passwordPanel"
-          :info="userInfo"
-          @showSnackbar="snackbar"
-        />
-        <v-card class="ma-2 pa-2">
-          <v-card-title>管理您的组队码</v-card-title>
-          <v-card-subtitle>
-            当您参加团队竞赛时，队长将通过您的组队码进行组队。<br />
-            请妥善保管组队码，如果您怀疑组队码已泄露，可随时重新生成。组队码也将在组队成功后自动重新生成。
-          </v-card-subtitle>
-          <v-card-title v-if="!showGroupCode" style="font-weight: 800"
-            >········
-          </v-card-title>
-          <v-card-title v-else style="font-weight: 800"
-            >{{ userInfo.groupCode || "您还没有组队码" }}
-          </v-card-title>
-          <v-card-actions>
-            <v-btn
-              v-if="!showGroupCode"
-              class="info"
-              @click="showGroupCode = true"
-              >查看组队码</v-btn
-            >
-            <v-btn v-else class="info" @click="showGroupCode = false"
-              >隐藏组队码</v-btn
-            >
-            <v-spacer></v-spacer>
-            <v-btn
-              class="warning"
-              @click="refreshGroupCode"
-              :loading="isReloadingGroupCode"
-              >刷新组队码</v-btn
-            >
-          </v-card-actions>
-        </v-card>
-      </v-container>
-      <v-container v-if="page === 'contest' && contestInfo.length !== 0">
-        <contest-info-bar
-            v-for="item in contestInfo"
-            :info="item"
-            :key="item.id"
+      <div v-if="isLoading" style="margin: 0; background: white; width: 100%; height: 80px">
+        <v-skeleton-loader type="image" class="my-5"></v-skeleton-loader>
+        <v-skeleton-loader type="heading" class="my-2"></v-skeleton-loader>
+        <v-skeleton-loader type="list-item-avatar-three-line@2">
+        </v-skeleton-loader>
+      </div>
+      <div v-else>
+        <v-container v-if="page === 'info'">
+          <user-card :info="userInfo" @showSnackbar="snackbar"></user-card>
+          <user-info-manager
+            v-if="panels.infoPanel"
+            :info="userInfo"
             @showSnackbar="snackbar"
           />
-      </v-container>
-      <v-container v-if="page === 'contest' && contestInfo.length === 0">
-        <div>暂时没有报名记录，快去报名竞赛吧！</div>
-      </v-container>
-      <v-container v-if="page === 'message'">
-        <message-center @showSnackbar="snackbar" />
-      </v-container>
+          <user-password-manager
+            v-if="panels.passwordPanel"
+            :info="userInfo"
+            @showSnackbar="snackbar"
+          />
+          <v-card class="ma-2 pa-2" v-if="userInfo.userType !== 'guest'">
+            <v-card-title>管理您的组队码</v-card-title>
+            <v-card-subtitle>
+              当您参加团队竞赛时，队长将通过您的组队码进行组队。<br />
+              请妥善保管组队码，如果您怀疑组队码已泄露，可随时重新生成。组队码也将在组队成功后自动重新生成。
+            </v-card-subtitle>
+            <v-card-title v-if="!showGroupCode" style="font-weight: 800"
+              >········
+            </v-card-title>
+            <v-card-title v-else style="font-weight: 800"
+              >{{ userInfo.groupCode || "您还没有组队码" }}
+            </v-card-title>
+            <v-card-actions>
+              <v-btn
+                v-if="!showGroupCode"
+                class="info"
+                @click="showGroupCode = true"
+                >查看组队码</v-btn
+              >
+              <v-btn v-else class="info" @click="showGroupCode = false"
+                >隐藏组队码</v-btn
+              >
+              <v-spacer></v-spacer>
+              <v-btn
+                class="warning"
+                @click="refreshGroupCode"
+                :loading="isReloadingGroupCode"
+                >刷新组队码</v-btn
+              >
+            </v-card-actions>
+          </v-card>
+        </v-container>
+        <v-container v-if="page === 'contest' && contestInfo.length !== 0">
+          <contest-info-bar
+              v-for="item in contestInfo"
+              :info="item"
+              :key="item.id"
+              @showSnackbar="snackbar"
+            />
+        </v-container>
+        <v-container v-if="page === 'contest' && contestInfo.length === 0">
+          <div>暂时没有报名记录，快去报名竞赛吧！</div>
+        </v-container>
+        <v-container v-if="page === 'message'">
+          <message-center @showSnackbar="snackbar" />
+        </v-container>
+      </div>
     </div>
   </div>
 </template>
@@ -164,6 +172,7 @@ export default {
             this.userInfo = res.data;
             console.log("Get User Info: ");
             console.log(this.userInfo);
+            this.isLoading = false;
           } else if(res.data.error === "login"){
             this.clearUserInfo();
           } else {
@@ -210,6 +219,8 @@ export default {
   },
   data() {
     return {
+      isLoading: true,
+
       // 面板控制相关
       panels: {
         passwordPanel: false,
