83859fdfdb13621071732248f9e05ad936a2da32
cobs14 <44751106+cobs14@users.noreply.github.com>
2020-12-24 06:03:39 +0800

    offline register

--- miniprogram/pages/register/register.js
+++ miniprogram/pages/register/register.js
@@ -3,8 +3,10 @@ const app = getApp()
 
 Page({
   data: {
+    selectedContest: -1,
     username: '',
     password: '',
+    isLoading: false,
     isLoggingIn: false,
     loggedIn: false,
     userInfo: {
@@ -27,6 +29,56 @@ Page({
 
   },
 
+  addParticipant() {
+    //TODO: add a participant
+    var that = this;
+    if (that.data.isLoading) {
+      console.log('is already loading');
+      return;
+    }
+    wx.scanCode({
+      //onlyFromCamera: true, // 只允许从相机扫码
+      success(res) {
+        let verifyCode = res.result.split('/apply/')[1];
+        if (!(typeof verifyCode == 'string')) {
+          app.showError('未找到有效信息');
+          return;
+        }
+        app.showLoading('正在查询');
+        that.setData({
+          isLoading: true,
+        })
+        console.log('what is sent?',  that.data.userInfo.session_id, that.data.selectedContest, verifyCode);
+        wx.request({
+          url: 'https://contestplus.cn/api/offline',
+          method: 'POST',
+          data: {
+            session_id: that.data.userInfo.session_id,
+            contestId: that.data.selectedContest,
+            qrcode: verifyCode,
+          },
+          success: function (res) {
+            console.log('success', res);
+            if (res.data.error != undefined) {
+              app.showError('操作失败');
+              return;
+            }
+            app.showMessage('已将' + (res.data.trueName ? res.data.trueName : res.data.username) + '添加到竞赛', 'none');
+          },
+          fail: function () {
+            app.showError('操作失败');
+            return;
+          },
+          complete: function () {
+            that.setData({
+              isLoading: false,
+            })
+            app.hideLoading();
+          }
+        })
+      }
+    })
+  },
 
   // 获取输入账号 
   usernameInput: function (e) {
@@ -40,6 +92,16 @@ Page({
     this.setData({
       password: e.detail.value
     })
+  },
+
+  showDetailed(event) {
+    let that = this;
+    let index = event.currentTarget.dataset.num;
+    that.setData({
+      selectedContest: that.data.userInfo.contestList[index].id == that.data.selectedContest ?
+        -1 : that.data.userInfo.contestList[index].id,
+    })
+    return;
   },
 
   // 登录处理
