a1f68ccbbe238095ea1a9c0bd69191191475a0a8
cobs14 <44751106+cobs14@users.noreply.github.com>
2020-12-23 19:18:14 +0800

    scan code test

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -10,7 +10,7 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   //事件处理函数
-  bindViewTap: function() {
+  bindViewTap: function () {
     wx.navigateTo({
       url: '../logs/logs'
     })
@@ -21,7 +21,7 @@ Page({
         userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
@@ -43,12 +43,27 @@ Page({
       })
     }
   },
-  getUserInfo: function(e) {
+  getUserInfo: function (e) {
     console.log(e)
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
+    })
+  },
+
+  redirect: function () {
+    var that = this;
+    wx.scanCode({
+      onlyFromCamera: true, // 只允许从相机扫码
+      success(res) {
+        console.log("扫码成功：" + JSON.stringify(res))
+
+        // 扫码成功后  在此处理接下来的逻辑
+        that.setData({
+          scanCode: res.result
+        })
+      }
     })
   }
 })
