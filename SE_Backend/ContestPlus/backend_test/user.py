2a853bfed9760aea87cfad59fcbfafc6d01beb02
Lucius <souzou@foxmail.com>
2020-12-24 04:59:56 +0800

    Update

---
+++ SE_Backend/ContestPlus/backend_test/user.py
@@ -0,0 +1,20 @@
+from django.test import TestCase
+from ContestPlus.models import User
+
+
+class TestUnit(TestCase):
+    def setUp(self):
+        pass
+
+    def test_function_successful(self):
+        # 使用被测试提供的类进行一定操作
+        # 。。。。。。
+        # 验证操作后产生的结果
+        # 。。。。。。
+
+    # 错误处理测试
+    def test_function_dail(self):
+        # 使用被测试的API进行一定操作，产生预期中的错误。
+        # 。。。。。。
+        # 检查错误处理的正确性
+        # 。。。。。。
