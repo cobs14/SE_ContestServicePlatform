8e15d3e432ec4526e56e9d878f765560149f07ae
Lucius <souzou@foxmail.com>
2020-12-24 04:35:19 +0800

    Revert "Update"
    This reverts commit f7c528d572552acf7231a89b6fe566c4609910a9.

--- SE_Backend/ContestPlus/backend_code/user.py
+++ SE_Backend/ContestPlus/backend_code/user.py
@@ -1,10 +1,12 @@
 from django.http import JsonResponse
 from ContestPlus.backend_code.secure import *
 from django.db.models import Q
+from SE_Backend import settings
 import datetime
 import threading
 import os
 import hashlib
+import qrcode
 
 
 def api_user_contact(request):
